b7c85beb43a5f25d918dc4bbc8701c40e654192a
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2018-09-08 18:47:44 -0500

    first commit- base project

---
+++ src/main/java/edu/eci/arsw/cinema/model/CinemaFunction.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.cinema.model;
+
+import edu.eci.arsw.cinema.persistence.CinemaException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author cristian
+ */
+public class CinemaFunction {
+    
+    private Movie movie;
+    private List<List<Boolean>> seats=new ArrayList<>();
+    private Date date;
+    
+    public CinemaFunction(){}
+    
+    public CinemaFunction(Movie movie, Date date){
+        this.movie=movie;
+        this.date=date;
+        List<Boolean> row= new ArrayList<>(Arrays.asList(new Boolean[10]));
+        Collections.fill(row, Boolean.FALSE);
+        for (int i=0;i<7;i++){
+            this.seats.add(row);
+        }
+    }
+    
+    public void buyTicket(int row,int col) throws CinemaException{
+        if (seats.get(row).get(col).equals(false)){
+            seats.get(row).set(col,Boolean.TRUE);
+        }
+        else{
+            throw new CinemaException("Seat booked");
+        }
+    }
+    
+    public List<List<Boolean>> getSeats() {
+        return this.seats;
+    }
+    
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/edu/eci/arsw/cinema/persistence/impl/InMemoryCinemaPersistence.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.cinema.persistence.impl;
+
+import edu.eci.arsw.cinema.model.Cinema;
+import edu.eci.arsw.cinema.model.CinemaFunction;
+import edu.eci.arsw.cinema.model.Movie;
+import edu.eci.arsw.cinema.persistence.CinemaException;
+import edu.eci.arsw.cinema.persistence.CinemaPersistenceException;
+import edu.eci.arsw.cinema.persistence.CinemaPersitence;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author cristian
+ */
+public class InMemoryCinemaPersistence implements CinemaPersitence{
+    
+    private final Map<String,Cinema> cinemas=new HashMap<>();
+
+    public InMemoryCinemaPersistence() {
+        //load stub data
+        Date functionDate = new GregorianCalendar(2018, 9, 25).getTime();
+        List<CinemaFunction> functions= new ArrayList<>();
+        CinemaFunction funct1 = new CinemaFunction(new Movie("SuperHeroes Movie","Action"),functionDate);
+        CinemaFunction funct2 = new CinemaFunction(new Movie("The Night","Horror"),functionDate);
+        functions.add(funct1);
+        functions.add(funct2);
+        Cinema c=new Cinema("cinemaX",functions);
+        cinemas.put("cinemaX", c);
+    }    
+
+    @Override
+    public void buyTicket(int row, int col, String cinema, Date date, String movieName) throws CinemaException {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public List<CinemaFunction> getFunctionsbyCinemaAndDate(String cinema, Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void saveCinema(Cinema c) throws CinemaPersistenceException {
+        if (cinemas.containsKey(c.getName())){
+            throw new CinemaPersistenceException("The given cinema already exists: "+c.getName());
+        }
+        else{
+            cinemas.put(c.getName(),c);
+        }   
+    }
+
+    @Override
+    public Cinema getCinema(String name) throws CinemaPersistenceException {
+        return cinemas.get(name);
+    }
+
+}
